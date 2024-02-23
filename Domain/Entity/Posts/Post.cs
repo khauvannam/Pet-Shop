d50b6285f157f2565de432ab8de600f9275e2a21
khauvannam <nkhauvan@gmail.com>
2024-02-23 21:38:14 +0700

    Initial commit

---
+++ Domain/Entity/Posts/Post.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entity.Posts;
+
+public class Post
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public float Price { get; set; }
+    public string Size { get; set; }
+}
